294ea8f14bec498ecb96efc09c4475a6f230e126
DannaCruz0055 <143195392+DannaCruz0055@users.noreply.github.com>
2023-11-28 22:30:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,14 +17,20 @@
                 <td>ID</td>
                 <td>Subject Code</td>
                 <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
             <?php
                 while($res = mysqli_fetch_array($result)) {
                     echo "<tr>";
-                    echo "<td>".$res['subjec_id']."</td>";
-                    echo "<td>".$res['subjec_code']."</td>";
-                    echo "<td>".$res['subjec_name']."</td>";
+                    echo "<td>".$res['subject_id']."</td>";
+                    echo "<td>".$res['subject_code']."</td>";
+                    echo "<td>".$res['subject_name']."</td>";
+                    echo "<td>".$res['syllabus_id']."</td>";
+                    echo "<td>".$res['syllabus_code']."</td>";
+                    echo "<td>".$res['syllabus_author']."</td>";
                     echo "<td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
                             <a href=\"functions/delete.php?id=$res[subject_id]\"
                             onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";

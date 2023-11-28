51b2cebcb629cd181d6905c2b1d41f83b643202c
DannaCruz0055 <143195392+DannaCruz0055@users.noreply.github.com>
2023-11-28 22:30:28 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,5 @@
 <?php
     //fetching data
-    $query = "SELECT * FROM tblsubject";
+    $query = "SELECT * FROM tblsubject LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubject.subjectid";
     $result = mysqli_query($dbc, $query); //using mysqli_query
 ?> 
